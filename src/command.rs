9be499d131f511b9b3ec8f729304e249891fb5c1
Cyber Phantom <inam123451@gmail.com>
2025-02-16 17:18:09 +0530

    Add definition for `Command` trait

---
+++ src/command.rs
@@ -0,0 +1,34 @@
+use super::types::{CommandId, EEPROMAddress};
+
+/*
+| Command ID | Command Status | EEPROM Address | Data Valid Length |     Data     | Checksum |
+|------------|----------------|----------------|-------------------|--------------|----------|
+|   1 Byte   |     1 Byte     |     2 Bytes    |       1 Byte      |   10 Bytes   |  1 Byte  |
+|------------|----------------|----------------|-------------------|--------------|----------|
+BASE_OFFSET: The offset of the first byte of the data field
+*/
+pub trait Command {
+    fn base_offset(&self) -> usize;
+
+    fn id(&self) -> CommandId;
+
+    fn set_id(&mut self, id: CommandId);
+
+    fn status(&self) -> u8;
+
+    fn set_status(&mut self, status: u8);
+
+    fn eeprom_address(&self) -> EEPROMAddress;
+
+    fn set_eeprom_address(&mut self, address: EEPROMAddress);
+
+    fn valid_data_len(&self) -> u8;
+
+    fn set_valid_data_len(&mut self, len: u8);
+
+    fn checksum(&self) -> u8;
+
+    fn set_checksum(&mut self, checksum: u8);
+
+    fn as_bytes(&mut self) -> &[u8];
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod command;
+mod types;
+
+pub use command::Command;
+pub use types::{CommandId, EEPROMAddress};
